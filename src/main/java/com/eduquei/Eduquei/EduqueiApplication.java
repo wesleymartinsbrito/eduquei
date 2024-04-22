d8822cf07082dcc64ef94e3f32673ef89f64ea80
Wesley Martins <113721470+wesleymartinsbrito@users.noreply.github.com>
2024-04-22 20:13:09 -0300

    First commit

---
+++ src/main/java/com/eduquei/Eduquei/EduqueiApplication.java
@@ -0,0 +1,13 @@
+package com.eduquei.Eduquei;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EduqueiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduqueiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eduquei/Eduquei/EduqueiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduquei.Eduquei;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduqueiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
