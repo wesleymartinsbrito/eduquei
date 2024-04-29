b87379e498b481cfb583225e560fa308d410e11c
Wesley Martins <113721470+wesleymartinsbrito@users.noreply.github.com>
2024-04-29 16:42:56 -0300

    findAll and findById created in service

--- src/main/java/com/eduquei/Eduquei/services/AlunoService.java
+++ src/main/java/com/eduquei/Eduquei/services/AlunoService.java
@@ -1,12 +1,27 @@
 package com.eduquei.Eduquei.services;
 
+import com.eduquei.Eduquei.entities.Aluno;
 import com.eduquei.Eduquei.repositories.AlunoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class AlunoService {
 
     @Autowired
     private AlunoRepository alunoRepository;
+
+    @Transactional(readOnly = true)
+    public Aluno findById(Long id) {
+        return alunoRepository.findById(id).get();
+    }
+
+    @Transactional(readOnly = true)
+    public List<Aluno> findAll(){
+        List<Aluno> alunos = alunoRepository.findAll();
+        return alunos;
+    }
 }
