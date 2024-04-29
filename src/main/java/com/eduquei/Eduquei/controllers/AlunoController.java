546b2753b05b4baa9f38857015f009eaa80cbe22
Wesley Martins <113721470+wesleymartinsbrito@users.noreply.github.com>
2024-04-29 16:48:11 -0300

    findAll and findById add in controller

--- src/main/java/com/eduquei/Eduquei/controllers/AlunoController.java
+++ src/main/java/com/eduquei/Eduquei/controllers/AlunoController.java
@@ -1,9 +1,14 @@
 package com.eduquei.Eduquei.controllers;
 
+import com.eduquei.Eduquei.entities.Aluno;
 import com.eduquei.Eduquei.services.AlunoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/alunos")
@@ -11,4 +16,15 @@ public class AlunoController {
 
     @Autowired
     private AlunoService alunoService;
+
+    @GetMapping(value = "/{id}")
+    public Aluno findById(@PathVariable Long id){
+        return alunoService.findById(id);
+    }
+
+    @GetMapping
+    public List<Aluno> findAll(){
+        List<Aluno> alunos = alunoService.findAll();
+        return alunos;
+    }
 }
