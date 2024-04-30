a82f7d4339a74ce02da7bbbe30dfda629ed3ff39
Wesley Martins <113721470+wesleymartinsbrito@users.noreply.github.com>
2024-04-30 18:27:59 -0300

    Turma repository created

---
+++ src/main/java/com/eduquei/Eduquei/repositories/TurmaRepository.java
@@ -0,0 +1,7 @@
+package com.eduquei.Eduquei.repositories;
+
+import com.eduquei.Eduquei.entities.Turma;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TurmaRepository extends JpaRepository<Turma, Long> {
+}
