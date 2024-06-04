9f8377aaa6802962da2efe1c85539026140970ed
Wesley Martins <wesleymartinsjnr@gmail.com>
2024-06-04 18:49:00 -0300

    authorizeHttpRequests config

--- src/main/java/com/eduquei/Eduquei/infra/security/SecurityConfiguration.java
+++ src/main/java/com/eduquei/Eduquei/infra/security/SecurityConfiguration.java
@@ -2,6 +2,7 @@ package com.eduquei.Eduquei.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -15,6 +16,11 @@ public class SecurityConfiguration {
         return httpSecurity
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/alunos").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.DELETE, "/alunos").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
                 .build();
     }
 }
